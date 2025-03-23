bdc4be54baa1b25232c4379db9130b56f5dbdab9
SHUBHAM SURANA <shubhamsurana1300@gmail.com>
2025-03-23 18:39:54 +0530

    Update ObserverApplication.java

--- observer/ObserverApplication.java
+++ observer/ObserverApplication.java
@@ -3,9 +3,15 @@ public class ObserverApplication {
 
 	public static void main(String[] args) {
 		NewsAgency observable = new NewsAgency();
-		NewsChannel observer = new NewsChannel();
+		
+		NewsChannel observer1 = new NewsChannel();
+		SportsChannel observer2 = new SportsChannel();
 
-		observable.addObserver(observer);
+		// Adding the subscriber 
+		observable.addObserver(observer1);
+		observable.addObserver(observer2);
+
+		// Now both above class will be notofied and news will be set
 		observable.setNews("news");
 		System.out.println(observer.getNews());
 	}
