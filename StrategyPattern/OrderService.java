856536d0c51b986375072f8946009c1c84fbd3a6
SHUBHAM SURANA <shubhamsurana1300@gmail.com>
2025-03-23 19:11:44 +0530

    Update OrderService.java

--- StrategyPattern/OrderService.java
+++ StrategyPattern/OrderService.java
@@ -11,7 +11,9 @@ public class OrderService {
 
   
     public double calculateFinalAmount(String strategyName, double orderAmount) {
+        // Below Factory Pattern helps create the right object.
         DiscountStrategy strategy = discountStrategyFactory.getStrategy(strategyName);
+        //Strategy allows different behaviors (algorithms) in those objects.
         return strategy.applyDiscount(orderAmount);
     }
 }
