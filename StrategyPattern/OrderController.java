d7b4a6b097447ba912e82cd1e7e13e1e1f53c913
SHUBHAM SURANA <shubhamsurana1300@gmail.com>
2025-03-23 18:15:33 +0530

    Update OrderController.java

--- StrategyPattern/OrderController.java
+++ StrategyPattern/OrderController.java
@@ -1,1 +1,25 @@
+/**
+ * REST Controller to expose discount calculation API.
+ */
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    /**
+     * http://localhost:8080/api/orders/discount?strategyName=FLAT&orderAmount=1000
+     * http://localhost:8080/api/orders/discount?strategyName=CASHBACK&orderAmount=1000
+     */
+    @GetMapping("/discount")
+    public double getDiscountedAmount(@RequestParam String strategyName,
+                                      @RequestParam double orderAmount) {
+        return orderService.calculateFinalAmount(strategyName, orderAmount);
+    }
+}
 
